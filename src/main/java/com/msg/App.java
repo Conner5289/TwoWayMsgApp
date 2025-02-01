f3ee6b362fc4dbaa12c123b46758543e530ce53d
Conner Yule <conneryule@gmail.com>
2025-01-31 22:34:07 -0500

    working client

--- src/main/java/com/msg/App.java
+++ src/main/java/com/msg/App.java
@@ -1,9 +1,16 @@
 package com.msg;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
 
 		ServerConnection server = new ServerConnection();
+
+		Scanner scnr = new Scanner(System.in);
+		String clientIp = scnr.next();
+
+		ClientConnection client = new ClientConnection(clientIp);
 
 	}
 }
--- src/main/java/com/msg/ClientConnection.java
+++ src/main/java/com/msg/ClientConnection.java
@@ -1,5 +1,22 @@
 package com.msg;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientConnection {
+
+	public ClientConnection(String ip) {
+		final int PORT = 5289;
+
+		try {
+			Socket clientSocket = new Socket(ip, PORT);
+			System.out.println("Client Socket connection on \nIp: " + ip + "\nPort: " + PORT);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Bad Client Socket");
+		}
+
+	}
 
 }
